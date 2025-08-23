991567f7aa4383b60df8b6e850de4c5a9e2aa5d6
Tushar Bhatt <tusharbhatt0135@gmail.com>
2025-08-23 23:28:13 +0530

    unwanted components

--- jamp-frontend/app/components/homepage/section-four.tsx
+++
@@ -1,6 +0,0 @@
-import type { SectionTwo } from '@/app/sanity/types'
-import VerticalSection from '../ui.tsx/listing'
-
-export default function SectionFour({ data }: { data: SectionTwo }) {
-    return <VerticalSection data={data}/>
-}
