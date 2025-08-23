7dcc6afd5f862154e5b7eeba7d692b1861c75787
Tushar Bhatt <tusharbhatt0135@gmail.com>
2025-08-24 00:08:50 +0530

    fix listing height issue

--- jamp-frontend/app/components/ui.tsx/listing.tsx
+++ jamp-frontend/app/components/ui.tsx/listing.tsx
@@ -9,11 +9,13 @@ export function Listing({ data }: { data: Listing[] }) {
         <div className='bg-roboto-background p-4 flex flex-col items-center gap-7'>
             {data?.map(({ _key, heading, items }) => (
                 <div key={_key} className='space-y-5'>
-                    <h2 className='text-center'>{heading}</h2>
+                    <h2 className='text-center text-lg'>{heading}</h2>
                     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 items-center sm:gap-7'>
                         {items.map(({ image, title, subtitle, aspectRatio }, i) => (
                             <div key={`${title}- ${i}`} className='text-roboto-gray text-center flex flex-col'>
-                                <Image src={urlFor(image).url()} className={`aspect-${aspectRatio} mb-1`} alt={title} height={250} width={250} />
+                                <div className='h-72 flex items-center'>
+                                <Image src={urlFor(image).url()} className={`aspect-[${aspectRatio}] mb-1`} alt={title} height={250} width={250} />
+                               </div>
                                 <p className='font-semibold'>{title}</p>
                                 <p>{subtitle}</p>
                             </div>
